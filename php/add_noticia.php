9ee11cddf6e82347d6f51c21c7af3b2a2fb65a97
ByTolinha <108443821+ByTolinha@users.noreply.github.com>
2023-09-04 11:34:30 -0300

    Add files via upload

--- php/add_noticia.php
+++ php/add_noticia.php
@@ -7,13 +7,14 @@
 	<script src="../js/jquery-3.6.0.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="../css/bootstrap-5.2.2-dist/css/bootstrap.min.css">
 	<script type="text/javascript" src="../css/bootstrap-5.2.2-dist/js/bootstrap.bundle.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
 	<script type="text/javascript">
 		$(document).ready(function(){
 			$("#enviar").click(function(){
   			$.ajax({
   				url: "script_noticia.php",
   				type: "POST",
-  				data: "nome="+$("#nome").val()+"&descricao="+$("#descricao").val()+"&curtida="+$("#curtida").val()+"&categoria="+$("#categoria").val(),
+  				data: "nome="+$("#nome").val()+"&descricao="+$("#descricao").val()+"&img="+$("#img").val()+"&categoria="+$("#categoria").val(),
   				dataType: "html"
   			}).done(function(resposta) {
 	    $("p").html(resposta);
@@ -29,25 +30,44 @@
 	</script>
 </head>
 <body>
-	<div class="container col-6">
-		<input class="form-control" type="text" id="nome">
-		<input class="form-control" type="text" id="descricao">
-		<input class="form-control" type="number" id="curtida">
-		<select class="form-control" id="categoria">
-		<option disabled selected>Selecione a categoria</option>
-		<?php
-		require('conecta.php');
-		$listagem = $conn->prepare('SELECT * FROM tb_categoria');
-		$listagem->execute();
-		while ($lista = $listagem->fetch(PDO::FETCH_ASSOC)) {
-		?>
-			<option value="<?php echo $lista['id']?>"><?php echo $lista['nm_categoria']?></option>
-		<?php
-		}
-		?>
-		</select>
-		<button id="enviar" class="btn btn-light">Enviar</button>
+	<div class="container bg-light mt-5">
+	<h2><i>Crie sua nova postagem</i></h2>
+		<div id="cadastro" class="container">
+			<div class="group1 col-20">
+				<input class="form-control" type="text" id="nome">
+				<input class="form-control" type="text" id="descricao">
+			</div>
+
+			<div class="group container col-8">
+				<select class="form-control" id="categoria">
+				<option disabled selected>Selecione a categoria</option>
+				<?php
+					require('conecta.php');
+					$listagem = $conn->prepare('SELECT * FROM tb_categoria');
+					$listagem->execute();
+					while ($lista = $listagem->fetch(PDO::FETCH_ASSOC)) {
+				?>
+				<option value="<?php echo $lista['id']?>"><?php echo $lista['nm_categoria']?></option>
+				<?php
+					}
+				?>
+				</select>
+				<img id="imge" src="foto.png">
+				<input class="form-control m-2" type="file" id="img">
+				<button id="enviar" class="btn btn-light">Enviar</button>
+			</div>
+
+			</div>
 		<p></p>
 	</div>
+
+	<script>
+		const image = document.querySelector("#imge"),
+		input = document.querySelector("#img");
+
+		input.addEventListener("change", () => {
+			image.src = URL.createObjectURL(input.files[0]);
+		});
+	</script>
 </body>
 </html>
--- php/script_noticia.php
+++ php/script_noticia.php
@@ -1,11 +1,11 @@
 <?php 
 		require('conecta.php');
         try{
-		$stmt = $conn->prepare('INSERT INTO tb_noticia(nm_noticia,ds_noticia,nr_curtidas,data_post,hora_post,id_categoria) VALUES (:nm,:ds,:nr,NOW(),NOW(),:id)');
+		$stmt = $conn->prepare('INSERT INTO tb_noticia(nm_noticia,ds_noticia,img_1,data_post,hora_post,id_categoria) VALUES (:nm,:ds,:img,NOW(),NOW(),:id)');
 		$stmt->execute(array(
 			':nm' => $_POST['nome'],
 			':ds' => $_POST['descricao'],
-			':nr' => $_POST['curtida'],
+			':img' => $_POST['img'],
 			':id' => $_POST['categoria']
 		));
 	}catch (Exception $e){
