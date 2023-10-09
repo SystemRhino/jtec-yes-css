45ec7997043b8dddb044a7a8f9b094b7de6cc070
ByTolinha <108443821+ByTolinha@users.noreply.github.com>
2023-10-09 11:37:31 -0300

    Add files via upload

---
+++ css2/footer.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>titu lo</title>
+    <meta charset="utf8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>    
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<footer>
+        <div id="sobre">
+            <h3>Etec de Itanhaém</h3>
+            <p>Av. José Batista Campos, 1431 - Cidade Anchieta - Itanhaém - SP - CEP: 11740-000</p>
+            <h4>E-mail Informativo</h4>
+            <p></p>
+        </div>
+        <div id="coment">
+            <h3>Últimos Comentários</h3>
+        </div>
+    </footer>
+    
+</body>
+</html>
