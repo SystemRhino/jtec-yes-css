f5f848f0ebd7f063e721bbca92bf6e73ed605083
ByTolinha <108443821+ByTolinha@users.noreply.github.com>
2023-11-11 13:53:10 -0300

    Add files via upload

--- adm/index.php
+++ adm/index.php
@@ -4,7 +4,6 @@ if ($_SESSION['nivel'] != 1) {
 	header('location:../');
 }
 $dir = basename(__DIR__);
-include('../nav.php')
 ?>
 <!DOCTYPE html>
 <html>
@@ -15,10 +14,12 @@ include('../nav.php')
   <link rel="stylesheet" type="text/css" href="css/bootstrap-5.2.2-dist/css/bootstrap.min.css">
 </head>
 <body>
-
-<a href="categoria.php">Categoria</a>
-<a href="users.php">Users</a>
-<a href="noticia.php">Noticia</a>
+<?php include('../nav.php'); ?>
+<div class="container-fluid mt-2 shadow p-3">
+	<a class="btn btn-outline-primary shadow" href="categoria.php">Categoria</a>
+	<a class="btn btn-outline-primary shadow" href="users.php">Users</a>
+	<a class="btn btn-outline-primary shadow" href="noticia.php">Noticia</a>
+</div>
 
 </body>
 </html>
