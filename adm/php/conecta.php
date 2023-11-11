e36a6c1f06ad32cf5578b0c54df8ed1ec5e7f2f5
ByTolinha <108443821+ByTolinha@users.noreply.github.com>
2023-11-11 13:53:41 -0300

    Add files via upload

--- adm/php/conecta.php
+++ adm/php/conecta.php
@@ -1,6 +1,6 @@
 <?php 
 try {
-  $conn = new PDO('mysql:host=127.0.0.1:3310;dbname=jtec', 'root', 'usbw');
+  $conn = new PDO('mysql:host=127.0.0.1:3310;dbname=jtec', 'root', '');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
