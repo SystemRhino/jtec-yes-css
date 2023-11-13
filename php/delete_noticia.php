6587345c4a88faf8cab6ee84dcc85b84a8d149fc
SystemRhino <80726377+SystemRhino@users.noreply.github.com>
2023-11-12 22:29:50 -0300

    Add files via upload

--- php/delete_noticia.php
+++ php/delete_noticia.php
@@ -1,17 +1,13 @@
 <?php
 session_start();
-if ($_SESSION['nivel'] != 1) {
-	header('location:../');
-}else{
 		$id = $_GET['id'];
 		include('conecta.php');
 		try {
 		  $delete_noticia = $conn->prepare("DELETE FROM tb_noticia WHERE (`id` = '$id')");
 		  $delete_noticia->execute();
-		  
+		  header('location:../perfil.php');
 		} catch(PDOException $e) {
 		    echo $e;
 		}
-	}
 
 ?>
